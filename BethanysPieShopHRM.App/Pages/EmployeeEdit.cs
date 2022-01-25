6b324c12e883e77ed204e2af68d7504fcf2dee0d
Adam <adam@eastpoint.co.uk>
2022-01-25 17:12:11 +0000

    add edit page that shows form of user information from db

---
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
@@ -0,0 +1,42 @@
+﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeEdit
+    {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+        
+        [Inject]
+        public ICountryDataService CountryDataService { get; set; }
+
+        [Inject]
+        public IJobCategoryDataService JobCategoryDataService { get; set; }
+
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+        public List<Country> Countries { get; set; } = new List<Country>();
+        public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
+
+        protected string CountryId = string.Empty;
+        protected string JobCategoryId = string.Empty;
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            Countries = (await CountryDataService.GetAllCountries()).ToList();
+            JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
+
+            CountryId = Employee.CountryId.ToString();
+            JobCategoryId = Employee.JobCategoryId.ToString();
+        }
+    }
+}
